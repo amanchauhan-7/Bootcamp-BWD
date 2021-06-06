12d1694b5fb628d12c6ed617dba0c1dc95ba1712
amanchauhan-7 <85453337+amanchauhan-7@users.noreply.github.com>
2021-06-06 22:53:38 +0530

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <h1> BootCamp Project </h1>
+    </header>
+  );
+}
+
+export default Header;
---
+++ Note.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>JavaScript and React.js </h1>
+      <p>
+      First, let me introduce myself. My name is Aman Chauhan, and I’m 1st Year B-tech student with CSE(Data Science) Branch.
+      I am learning Web Development :)
+      </p>
+    </div>
+  );
+}
+
+export default Note;
